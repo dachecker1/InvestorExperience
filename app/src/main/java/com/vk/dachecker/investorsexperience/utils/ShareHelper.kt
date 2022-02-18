9910357e261641d7ff817148978c353dd24bf67c
dachecker1 <dachecker@ya.ru>
2022-02-18 17:08:27 +0300

    настроил рекламу баннер в кнопку "поделиться" положил ссылку на Google Play

--- app/src/main/java/com/vk/dachecker/investorsexperience/utils/ShareHelper.kt
+++ app/src/main/java/com/vk/dachecker/investorsexperience/utils/ShareHelper.kt
@@ -1,9 +1,12 @@
 package com.vk.dachecker.investorsexperience.utils
 
 import android.content.Intent
+import com.vk.dachecker.investorsexperience.R
 import com.vk.dachecker.investorsexperience.data.model.Company
 
 object ShareHelper {
+    const val GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.vk.dachecker.investorsexperience"
+
     fun shareTickerVideo(company: Company) : Intent {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plane"
@@ -18,7 +21,12 @@ object ShareHelper {
         stringBuilder.append(company.ticker)
         stringBuilder.append("\n")
         stringBuilder.append(company.url)
+        stringBuilder.append("\n")
+        stringBuilder.append("Качай приложение \"Опыт инвестора\" в Google Play")
+        stringBuilder.append("\n")
+        stringBuilder.append(GOOGLE_PLAY_URL)
 //        вписать ссылку на скачивание приложения с гугл плей.
         return stringBuilder.toString()
     }
+
 }
